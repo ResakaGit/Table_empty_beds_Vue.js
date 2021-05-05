0624245cc184ee8f805c2b7368ee20176417d8c4
Augusto G <agumza1@gmail.com>
2021-05-04 22:54:16 -0300

    filtros y total de camas

--- app3.js
+++ app3.js
@@ -3,8 +3,12 @@ new Vue({
     data () {
       return {
         Datos: [],
+        DatosCentro:[],
+        DatosNorte:[],
+        DatosDomiocilio:[],
         loading: true,
-        errored: false
+        errored: false,
+        CamasLiresTotal:0,
       }
     },
     SeñalarTabla () {
@@ -13,21 +17,56 @@ new Vue({
       })
     },
     methods:{
+      SumarCamasLibres(){
+        let CamasArray = 0;
+        for(let i=0; i < this.Datos.length; i++){
+          CamasArray = CamasArray + this.Datos[i].camaslibres;
+          this.CamasLiresTotal = CamasArray;  
+        }
+          
+      },
     GetforAxios(){
-      axios.get('http://192.168.1.42:3300/api/departments')
+      axios.get('http://192.168.1.43:3300/api/events')
         .then(response => {
           this.Datos = response.data,
-          console.log("JASON: ",this.Datos )
+          this.SumarCamasLibres();
+          this.ContructorDatosCentro();
+          this.ContructorDatosNorte();
+          this.ContructorDatosDomicilio();
         })
         .catch(error => {
           console.log(error)
           this.errored = true
         })
-        .finally(() => this.loading = false)
-      }
+        .finally(() => this.loading = false);
+        
+      },
+      ContructorDatosCentro(){
+        let NewArray = this.Datos.filter(ClinicaCentro => {
+          return ClinicaCentro.ubic_nombre === "CLÍNICA ESPERANZA CENTRO";
+        })
+        this.DatosCentro = NewArray;
+        // console.log("DATOS CENTRO",this.DatosCentro);
+
+      },
+      ContructorDatosNorte(){
+        let NewArray = this.Datos.filter(ClinicaNorte => {
+          return ClinicaNorte.ubic_nombre === "CLÍNICA ESPERANZA NORTE";
+        })
+        this.DatosNorte = NewArray;
+        // console.log("Norte",this.DatosNorte);
+      },
+      ContructorDatosDomicilio(){
+        let NewArray = this.Datos.filter(DatosDomiocilio => {
+          return DatosDomiocilio.ubic_nombre === "INTERNACION DOMICILIARIA";
+        })
+        this.DatosDomiocilio = NewArray;
+        // console.log("DATOS ais",this.DatosDomiocilio);
+      },
     },
     created () {
-        this.GetforAxios()
+        this.GetforAxios(),
         this.SeñalarTabla()
+        
       }
   })
